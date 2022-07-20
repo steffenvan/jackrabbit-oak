5d497ada86207d0f05a420039c88bb33b32f090b
Steffen Van <svan@adobe.com>
2022-07-20 13:25:55 +0200

    feature: added CMS test

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketchTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketchTest.java
@@ -6,39 +6,72 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CountMinSketchTest {
 
     @Test
-    public void testAdd() {
-        CountMinSketch sketch = new CountMinSketch(rows(3), cols(50));
+    public void testPrecision() {
+        int numData = 1000000;
+        int[] data = getRandomData(numData);
+
+        double epsilon = 0.001;
+        double delta = 0.99;
+        CountMinSketch sketch = new CountMinSketch(epsilon, delta);
         sketch.setSeed(0);
 
-        Random r = new Random();
-        int[] data = new int[100];
-        for (int i = 0; i < data.length; i++) {
-            data[i] = r.nextInt();
-            System.out.println(data[i]);
-        }
-
+        // add to the CMS
         for (int datum : data) {
             sketch.add(Hash.hash64(datum));
         }
-        System.out.println(Arrays.deepToString(sketch.getItems()));
 
+        // get the actual counts of each data point to compare with the estimated one from the CMS.
+        Map<Integer, Integer> actualFrequencies = new HashMap<>();
+        for (int num : data) {
+            actualFrequencies.put(num, actualFrequencies.getOrDefault(num, 0) + 1);
+        }
 
+        int numErrors = 0;
+        // check that the error ratio is within the specified bounds
+        for (int num : data) {
+            double errorRatio = ((double) sketch.estimateCount(Hash.hash64(num)) - actualFrequencies.get(num)) / numData;
+            if (errorRatio >= epsilon){
+                numErrors++;
+            }
+        }
 
+        double correct = 1.0 - ((double) numErrors / actualFrequencies.size());
+        assertTrue("delta not reached: required " + delta + ", reached " + correct, correct > delta);
     }
 
-    private static int rows(int r) {
+    private int[] getRandomData(int numData) {
+        Random r = new Random();
+        int[] data = new int[numData];
+        int maxNum = 20;
+        for (int i = 0; i < data.length; i++) {
+            int num = r.nextInt(maxNum);
+            data[i] = r.nextInt(1 << num);
+        }
+        return data;
+    }
+
+    private int rows(int r) {
         return r;
     }
 
-    private static int cols(int c) {
+    private int cols(int c) {
         return c;
+    }
+
+    private double epsilon(double x) {
+        return x;
+    }
+
+    private double delta(double x) {
+        return x;
     }
 }
