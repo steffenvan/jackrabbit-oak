809521a48d23aa378878e92480e7fa80aeda401d
steffenvan <steffen@vanfamily.com>
2022-08-23 12:51:57 +0200

    chore: removed unecessary override of hashcode()

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/PropertyInfo.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/PropertyInfo.java
@@ -1,18 +1,9 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
-import java.util.Objects;
-
 public class PropertyInfo {
 
 	private final String name;
 	private long count;
-	private long hash64;
-
-	PropertyInfo(String name, long count, long hash64) {
-		this.name = name;
-		this.count = count;
-		this.hash64 = hash64;
-	}
 
 	PropertyInfo(String name, long count) {
 		this.name = name;
@@ -27,18 +18,8 @@ public class PropertyInfo {
 		return name;
 	}
 
-	public long getHash64() {
-		return hash64;
-	}
-
 	@Override
 	public String toString() {
-		return name + "->" + count;
+		return name + "=" + count;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(name, hash64);
-	}
-
 }
