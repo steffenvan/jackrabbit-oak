5388dd4d9c3ddbb13aa4fceb5f526fee1ab8175d
Thomas Mueller <thomasm@apache.org>
2022-10-19 16:16:23 +0200

    Fix HyperLogLog

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/HyperLogLog.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/HyperLogLog.java
@@ -1,89 +1,87 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class HyperLogLog implements CardinalityEstimator {
 
-	private final int m;
-	private byte[] counts;
-	private final double am;
+    private final int m;
+    private byte[] counters;
+    private final double am;
 
-	public HyperLogLog(int m) {
-		this(m, new byte[m]);
-	}
+    public HyperLogLog(int m) {
+        this(m, new byte[m]);
+    }
 
-	public HyperLogLog(int m, byte[] counts) {
-		this.m = m;
-		this.counts = counts;
-		switch (m) {
-		case 32:
-			this.am = 0.697;
-			break;
-		case 64:
-			this.am = 0.709;
-			break;
-		default:
-			this.am = 0.7213 / (1.0 + 1.079 / m);
-		}
+    public HyperLogLog(int m, byte[] counters) {
+        if (m < 16) {
+            throw new IllegalArgumentException("Must be >= 16, is " + m);
+        }
+        if (Integer.bitCount(m) != 1) {
+            throw new IllegalArgumentException("Must be a power of 2, is " + m);
+        }
+        this.m = m;
+        this.counters = counters;
+        switch (m) {
+        case 32:
+            this.am = 0.697;
+            break;
+        case 64:
+            this.am = 0.709;
+            break;
+        default:
+            this.am = 0.7213 / (1.0 + 1.079 / m);
+        }
+    }
 
-	}
+    @Override
+    public void add(long hash) {
+        int i = (int) (hash & (m - 1));
+        counters[i] = (byte) Math.max(counters[i], 1 + Long.numberOfLeadingZeros(hash));
+    }
 
-	@Override
-	public void add(long hash) {
-		int i = (int) (hash & (m - 1));
-		counts[i] = (byte) Math.max(counts[i], 1 + Long.numberOfLeadingZeros(hash));
-	}
+    @Override
+    public long estimate() {
+        double sum = 0;
+        int countZero = 0;
+        for (int c : counters) {
+            countZero += c == 0 ? 1 : 0;
+            sum += 1. / (1L << (c & 0xff));
+        }
+        long est = (long) (1. / sum * am * m * m);
+        if (est <= 5 * m && countZero > 0) {
+            // linear counting
+            est = (long) (m * Math.log((double) m / countZero));
+        }
+        return Math.max(1, est);
+    }
 
-	@Override
-	public long estimate() {
-		int numZeroes = 0;
-		double sum = 0.0;
-		for (int count : counts) {
-			sum += 1.0 / (1L << (count & 0xff));
-			if (count == 0) {
-				numZeroes++;
-			}
-		}
+    public String serialize() {
+        StringBuilder sb = new StringBuilder();
 
-		long estimate = (long) (1. / sum * m * m * am);
+        for (int i = 0; i < counters.length; i++) {
+            if (i > 0) {
+                sb.append(" ");
+            }
+            sb.append(counters[i]);
+        }
 
-		if (estimate <= 5 * m && numZeroes > 0) {
-			estimate = linearCount(numZeroes);
-		}
-		return Math.max(1, estimate);
-	}
+        return sb.toString();
+    }
 
-	private long linearCount(int numZeroes) {
-		return (long) (m * Math.log(m / numZeroes));
-	}
+    public static byte[] deserialize(String hllCounts) {
+        String[] parts = hllCounts.split("\\s+");
+        List<Byte> longList = Stream.of(parts).map(Byte::valueOf).collect(Collectors.toList());
+        byte[] data = new byte[parts.length];
+        for (int i = 0; i < longList.size(); i++) {
+            data[i] = longList.get(i);
+        }
+        return data;
+    }
 
-	byte[] getCounts() {
-		return Arrays.copyOf(counts, m);
-	}
+    public byte[] getCounters() {
+        return counters;
+    }
 
-	public String serialize() {
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < counts.length; i++) {
-			if (i > 0) {
-				sb.append(" ");
-			}
-			sb.append(counts[i]);
-		}
-
-		return sb.toString();
-	}
-
-	public static byte[] deserialize(String hllCounts) {
-		String[] parts = hllCounts.split("\\s+");
-		List<Byte> longList = Stream.of(parts).map(Byte::valueOf).collect(Collectors.toList());
-		byte[] data = new byte[parts.length];
-		for (int i = 0; i < longList.size(); i++) {
-			data[i] = longList.get(i);
-		}
-		return data;
-	}
 }
--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/HyperLogLogTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/HyperLogLogTest.java
@@ -1,5 +1,7 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Set;
@@ -27,12 +29,12 @@ public class HyperLogLogTest {
 
 		int actual = actualUniqueElements.size();
 		long estimated = hll.estimate();
-		double maxError = 1.3 / Math.sqrt(m);
-		double error = (double) estimated / actual;
-		System.out.println(error);
+		double maxError = 2 * (1.3 / Math.sqrt(m));
+		double error = Math.abs(((double) estimated / actual) - 1);
+		// System.out.println(error);
 		Assert.assertTrue("maxerror: " + maxError + " got: " + error, error <= maxError);
-		System.out.println(actualUniqueElements.size());
-		System.out.println(hll.estimate());
+		// System.out.println(actualUniqueElements.size());
+		// System.out.println(hll.estimate());
 	}
 
 	@Test
@@ -47,7 +49,7 @@ public class HyperLogLogTest {
 			hll.add(hash);
 		}
 
-		byte[] expected = hll.getCounts();
+		byte[] expected = hll.getCounters();
 		String s = hll.serialize();
 		byte[] actual = hll.deserialize(s);
 		Assert.assertArrayEquals(expected, actual);
@@ -64,4 +66,87 @@ public class HyperLogLogTest {
 		}
 		return data;
 	}
+	
+	
+	
+    @Test
+    public void test() {
+        int testCount = 50;
+        double avg = Math.sqrt(averageOverRange(30_000, testCount, false, 2));
+        double min = 15, max = 16;
+        // System.out.println(type + " expected " + min + ".." + max + " got " + avg);
+        assertTrue("expected " + min + ".." + max + " got " + avg, min < avg && avg < max);
+    }
+
+    private static double averageOverRange(long maxSize, int testCount, boolean debug, double exponent) {
+        double sum = 0;
+        int count = 0;
+        for (long size = 1; size <= 20; size++) {
+            sum += test(size, testCount, debug, exponent);
+            count++;
+        }
+        for (long size = 22; size <= 300; size += size / 5) {
+            sum += test(size, testCount, debug, exponent);
+            count++;
+        }
+        for (long size = 400; size <= maxSize; size *= 2) {
+            sum += test(size, testCount, debug, exponent);
+            count++;
+        }
+        return sum / count;
+    }
+
+    private static double test(long size, int testCount, boolean debug, double exponent) {
+        long x = 0;
+        long min = Long.MAX_VALUE, max = Long.MIN_VALUE;
+        long ns = System.nanoTime();
+        double sumSquareError = 0;
+        double sum = 0;
+        double sumFirst = 0;
+        int repeat = 10;
+        int runs = 2;
+        for (int test = 0; test < testCount; test++) {
+            CardinalityEstimator est = new HyperLogLog(32);
+            long baseX = x;
+            for (int i = 0; i < size; i++) {
+                est.add(Hash.hash64(x));
+                x++;
+            }
+            long e = est.estimate();
+            sum += e;
+            min = Math.min(min, e);
+            max = Math.max(max, e);
+            long error = e - size;
+            sumSquareError += error * error;
+            sumFirst += e;
+            for (int add = 0; add < repeat; add++) {
+                long x2 = baseX;
+                for (int i = 0; i < size; i++) {
+                    est.add(Hash.hash64(x2));
+                    x2++;
+                }
+            }
+            e = est.estimate();
+            sum += e;
+            min = Math.min(min, e);
+            max = Math.max(max, e);
+            error = e - size;
+            sumSquareError += error * error;
+        }
+        ns = System.nanoTime() - ns;
+        long nsPerItem = ns / testCount / runs / (1 + repeat) / size;
+        double stdDev = Math.sqrt(sumSquareError / testCount / runs);
+        double relStdDevP = stdDev / size * 100;
+        int biasFirstP = (int) (100 * (sumFirst / testCount / size) - 100);
+        int biasP = (int) (100 * (sum / testCount / runs / size) - 100);
+        if (debug) {
+            System.out.println("size " + size + " relStdDev% " + (int) relStdDevP + " range " + min + ".." + max
+                    + " testCount " + testCount + " biasFirst% " + biasFirstP + " bias% " + biasP + " avg "
+                    + (sum / testCount / runs) + " time " + nsPerItem);
+        }
+        // we try to reduce the relStdDevP, make sure there are no large values
+        // (trying to reduce sumSquareError directly
+        // would mean we care more about larger sets, but we don't)
+        return Math.pow(relStdDevP, exponent);
+    }
 }
