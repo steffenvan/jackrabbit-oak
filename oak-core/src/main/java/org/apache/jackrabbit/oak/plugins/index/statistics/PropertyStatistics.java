83589fc279bf5c60dc670c3ece5fab09ece2d2ad
steffenvan <svan@adobe.com>
2022-10-31 17:26:40 +0100

    feature: added more statistics

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/PropertyStatistics.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/PropertyStatistics.java
@@ -7,27 +7,35 @@ import org.apache.jackrabbit.oak.api.Type;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.slf4j.Logger;
 
+import static org.apache.jackrabbit.oak.plugins.index.statistics.PropertyReader.getLongOrZero;
+
 public class PropertyStatistics {
 
 	private final String name;
 	private long count;
-	private CountMinSketch valueSketch;
-	private TopKElements topKElements;
-	private HyperLogLog hll;
-	private long valueLength;
-
-	private long maxLength;
-	private long minLength;
+	private final CountMinSketch valueSketch;
+	private final TopKElements topKElements;
+	private final HyperLogLog hll;
+	private long valueLengthTotal;
+	private long valueLengthMax;
+	private long valueLengthMin;
 
 	PropertyStatistics(String name, long count, HyperLogLog hll, CountMinSketch valueSketch,
 			TopKElements topKElements) {
-        this.name = name;
-        this.count = count;
-        this.hll = hll;
+		this(name, count, hll, valueSketch, topKElements, 0, 0, Long.MAX_VALUE);
+
+	}
+
+	PropertyStatistics(String name, long count, HyperLogLog hll, CountMinSketch valueSketch,
+					   TopKElements topKElements, long valueLengthTotal, long valueLengthMax, long valueLengthMin) {
+		this.name = name;
+		this.count = count;
+		this.hll = hll;
 		this.valueSketch = valueSketch;
 		this.topKElements = topKElements;
-		this.maxLength = 0;
-		this.minLength = Long.MAX_VALUE;
+		this.valueLengthTotal = valueLengthTotal;
+		this.valueLengthMax = valueLengthMax;
+		this.valueLengthMin = valueLengthMin;
 	}
 
 	void updateHll(long hash) {
@@ -38,9 +46,9 @@ public class PropertyStatistics {
 		valueSketch.add(hash);
 		topKElements.update(val, valueSketch.estimateCount(hash));
 		long len = val.toString().length();
-		valueLength += len;
-		maxLength = Math.max(maxLength, len);
-		minLength = Math.min(minLength, len);
+		valueLengthTotal += len;
+		valueLengthMax = Math.max(valueLengthMax, len);
+		valueLengthMin = Math.min(valueLengthMin, len);
 	}
 
 	List<PropertyInfo> getTopKValuesDescending() {
@@ -56,38 +64,36 @@ public class PropertyStatistics {
 		PropertyState rowsProp = node.getProperty(rowName);
 		PropertyState colsProp = node.getProperty(colName);
 
-		int rows = rowsProp.getValue(Type.LONG).intValue();
-		int cols = colsProp.getValue(Type.LONG).intValue();
+		int rows = getLongOrZero(rowsProp).intValue();
+		int cols = getLongOrZero(colsProp).intValue();
 		long[][] data = new long[rows][cols];
 
-        if (rowsProp != null || colsProp != null) {
-            for (int i = 0; i < rows; i++) {
-                PropertyState ps = node.getProperty(cmsName + i);
-                if (ps != null) {
-                    String s = ps.getValue(Type.STRING);
-                    try {
-                        long[] row = CountMinSketch.deserialize(s);
-                        data[i] = row;
-                    } catch (NumberFormatException e) {
-                        logger.warn("Can not parse " + s);
-                    }
-                }
-            }
-        }
+		for (int i = 0; i < rows; i++) {
+			PropertyState ps = node.getProperty(cmsName + i);
+			if (ps != null) {
+				String s = ps.getValue(Type.STRING);
+				try {
+					long[] row = CountMinSketch.deserialize(s);
+					data[i] = row;
+				} catch (NumberFormatException e) {
+					logger.warn("Can not parse " + s);
+				}
+			}
+		}
 
 		return new CountMinSketch(rows, cols, data);
 	}
 
-	long getTotalValueLength() {
-		return valueLength;
+	long getValueLengthTotal() {
+		return valueLengthTotal;
 	}
 
-	long getMaxLength() {
-		return maxLength;
+	long getValueLengthMax() {
+		return valueLengthMax;
 	}
 
-	long getMinLength() {
-		return minLength;
+	long getValueLengthMin() {
+		return valueLengthMin;
 	}
 
 	long getCount() {
@@ -105,5 +111,4 @@ public class PropertyStatistics {
 	void inc(long count) {
 		this.count += count;
 	}
-
 }
