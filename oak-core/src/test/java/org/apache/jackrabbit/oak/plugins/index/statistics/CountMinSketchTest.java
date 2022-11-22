2218acbef993d4f11847d8010910814f74993b9a
steffenvan <svan@adobe.com>
2022-11-22 16:54:42 +0100

    chore: added a test case, changed existing a little

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketchTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketchTest.java
@@ -2,10 +2,8 @@ package org.apache.jackrabbit.oak.plugins.index.statistics;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,10 +15,12 @@ public class CountMinSketchTest {
 	// TODO: Improve tests
 	@Test
 	public void testPrecision() {
-		int numData = 1000000;
-		int[] data = getRandomData(numData);
-		int rows = 7;
-		int cols = 32;
+//		int numData = 1000000;
+//		int[] data = getRandomData(numData);
+		int[] data = getRandom();
+		int numData = data.length;
+		int rows = 5;
+		int cols = 8;
 		double epsilon = 2.0 / cols;
 		double delta = 1 - 1 / Math.pow(2, rows);
 		CountMinSketch sketch = new CountMinSketch(rows, cols);
@@ -43,12 +43,14 @@ public class CountMinSketchTest {
 		for (int num : data) {
 			double errorRatio = ((double) sketch.estimateCount(Hash.hash64(num)) - actualFrequencies.get(num))
 					/ numData;
+			System.out.println(errorRatio);
 			if (errorRatio >= epsilon) {
 				numErrors++;
 			}
 		}
 
 		double correct = 1.0 - ((double) numErrors / actualFrequencies.size());
+		System.out.println(correct);
 		assertTrue("delta not reached: required " + delta + ", reached " + correct, correct > delta);
 	}
 
@@ -62,17 +64,20 @@ public class CountMinSketchTest {
 		}
 		return data;
 	}
-
+	private int[] getRandom() {
+		return IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
+	}
 	@Test
 	public void testSerialization() {
 		int numData = 10;
-		int[] data = getRandomData(numData);
-
+//		int[] data = getRandomData(numData);
+		int[] data = getRandom();
+		System.out.println(Arrays.toString(data));
 		double epsilon = 0.001;
 		double delta = 0.99;
 
-		int rows = 3;
-		int cols = 10;
+		int rows = 5;
+		int cols = 8;
 		CountMinSketch sketch = new CountMinSketch(rows, cols);
 
 		for (int num : data) {
@@ -94,6 +99,34 @@ public class CountMinSketchTest {
 
 		for (int i = 0; i < expected.length; i++) {
 			Assert.assertArrayEquals(expected[i], actual[i]);
+		}
+	}
+	@Test
+	public void testPrecisionWithStrings() {
+		List<String> sameName = new ArrayList<>();
+		sameName.add("rep:isAllow");
+		sameName.add("ignoreData");
+		sameName.add("jcr:isAbstract");
+		sameName.add("collapsed");
+
+		CountMinSketch cms = new CountMinSketch(5, 16);
+		Set<Long> seen = new HashSet<>();
+		for (int i = 0; i < 1000; i++) {
+			for (String name : sameName) {
+				long properHash = Hash.hash64(name.hashCode());
+				if (seen.contains((long) name.hashCode()) || seen.contains(properHash)) {
+					System.out.println("ERROR");
+				}
+				seen.add((long) name.hashCode());
+				seen.add(properHash);
+				cms.add(properHash);
+			}
+			seen.clear();
+		}
+
+		for (String name : sameName) {
+			long properHash = Hash.hash64(name.hashCode());
+			assertTrue(cms.estimateCount(properHash) <= 1000 * 1.01);
 		}
 	}
 
