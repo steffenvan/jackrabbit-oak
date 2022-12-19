4ac0fc612b244da4da53a412df9520d9195f8f91
steffenvan <svan@adobe.com>
2022-12-19 13:27:09 +0100

    chore: added comments

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketch.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/CountMinSketch.java
@@ -14,13 +14,11 @@ import java.util.stream.Stream;
 import static org.apache.jackrabbit.oak.plugins.index.statistics.PropertyReader.getLongOrZero;
 
 public class CountMinSketch implements FrequencyCounter {
+	private static final Random RANDOM = new Random();
 	private final long[][] items;
 	private final int rows;
 	private final int cols;
-
 	private long count;
-
-	private static final Random RANDOM = new Random();
 
 	public CountMinSketch(int rows, int cols) {
 		this(rows, cols, new long[rows][cols]);
@@ -37,14 +35,54 @@ public class CountMinSketch implements FrequencyCounter {
 		this.items = items;
 	}
 
+	// Copy constructor
 	public CountMinSketch(CountMinSketch cms) {
 		this(cms.getRows(), cms.getCols(), cms.getData());
 	}
 
-	void setSeed(int seed) {
-		RANDOM.setSeed(seed);
+	public static long estimateCount(long hash, long[][] counts, int cols,
+									 int rows) {
+		long currMin = Long.MAX_VALUE;
+		for (int i = 0; i < rows; i++) {
+			long h2 = Hash.hash64(hash, i);
+			int col = (int) (h2 & (cols - 1));
+			currMin = Math.min(currMin, counts[i][col]);
+		}
+
+		return currMin;
 	}
 
+	public static long[] deserialize(String row) {
+		String[] allStuff = row.split("\\s+");
+		List<Long> longList = Stream.of(allStuff).map(Long::valueOf).collect(Collectors.toList());
+		return longList.stream().mapToLong(i -> i).toArray();
+	}
+
+	public static CountMinSketch readCMS(NodeState node, String cmsName,
+										 String rowName, String colName,
+										 Logger logger) {
+		PropertyState rowsProp = node.getProperty(rowName);
+		PropertyState colsProp = node.getProperty(colName);
+
+		int rows = getLongOrZero(rowsProp).intValue();
+		int cols = getLongOrZero(colsProp).intValue();
+		long[][] data = new long[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
+			PropertyState ps = node.getProperty(cmsName + i);
+			if (ps != null) {
+				String s = ps.getValue(Type.STRING);
+				try {
+					long[] row = CountMinSketch.deserialize(s);
+					data[i] = row;
+				} catch (NumberFormatException e) {
+					logger.warn("Can not parse " + s);
+				}
+			}
+		}
+
+		return new CountMinSketch(rows, cols, data);
+	}
 	public int getRows() {
 		return this.rows;
 	}
@@ -75,18 +113,6 @@ public class CountMinSketch implements FrequencyCounter {
 		return currMin;
 	}
 
-	public static long estimateCount(long hash, long[][] counts, int cols, int rows) {
-		long currMin = Long.MAX_VALUE;
-		int shift = Integer.bitCount(rows - 1);
-		for (int i = 0; i < rows; i++) {
-			long h2 = Hash.hash64(hash, i);
-			int col = (int) (h2 & (cols - 1));
-			currMin = Math.min(currMin, counts[i][col]);
-		}
-
-		return currMin;
-	}
-
 	public long[][] getData() {
 		return Arrays.stream(items).map(long[]::clone).toArray(long[][]::new);
 	}
@@ -108,35 +134,7 @@ public class CountMinSketch implements FrequencyCounter {
 		return asStrings;
 	}
 
-	public static long[] deserialize(String row) {
-		String[] allStuff = row.split("\\s+");
-		List<Long> longList = Stream.of(allStuff).map(Long::valueOf).collect(Collectors.toList());
-		return longList.stream().mapToLong(i -> i).toArray();
+	void setSeed(int seed) {
+		RANDOM.setSeed(seed);
 	}
-
-	public static CountMinSketch readCMS(NodeState node, String cmsName, String rowName, String colName,
-										 Logger logger) {
-		PropertyState rowsProp = node.getProperty(rowName);
-		PropertyState colsProp = node.getProperty(colName);
-
-		int rows = getLongOrZero(rowsProp).intValue();
-		int cols = getLongOrZero(colsProp).intValue();
-		long[][] data = new long[rows][cols];
-
-		for (int i = 0; i < rows; i++) {
-			PropertyState ps = node.getProperty(cmsName + i);
-			if (ps != null) {
-				String s = ps.getValue(Type.STRING);
-				try {
-					long[] row = CountMinSketch.deserialize(s);
-					data[i] = row;
-				} catch (NumberFormatException e) {
-					logger.warn("Can not parse " + s);
-				}
-			}
-		}
-
-		return new CountMinSketch(rows, cols, data);
-	}
-
 }
--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/FrequencyCounter.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/FrequencyCounter.java
@@ -1,7 +1,24 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
+/**
+ * Represents the approximate count of properties or values. We only count the
+ * values if their respective property exceeds a certain threshold.
+ *
+ */
 public interface FrequencyCounter {
 
+    /**
+     * Adds a certain property or value into the data structure.
+     *
+     * @param hash a 64 bit hash value.
+     */
     void add(long hash);
+
+    /**
+     * Gets the estimated count of the provided property or value.
+     *
+     * @param hash a 64 bit hash value.
+     * @return the estimated count of property or value.
+     */
     long estimateCount(long hash);
 }
