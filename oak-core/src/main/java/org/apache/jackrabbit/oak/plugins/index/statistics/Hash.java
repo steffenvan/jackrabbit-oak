2251390d2d2a709d5537927d3ee877e53b4819d2
steffenvan <svan@adobe.com>
2022-12-22 16:44:54 +0100

    refactor: removed redundant code

--- oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/Hash.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/plugins/index/statistics/Hash.java
@@ -1,17 +1,6 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
 public class Hash {
-
-    private static long seed;
-
-    public static void setSeed(long s) {
-        seed = s;
-    }
-
-    public static long randomLong() {
-        return hash64(seed++);
-    }
-
     public static long hash64(long x, long seed) {
         x += seed;
         x = (x ^ (x >>> 33)) * 0xff51afd7ed558ccdL;
@@ -19,22 +8,8 @@ public class Hash {
         x = x ^ (x >>> 33);
         return x;
     }
-
+    
     public static long hash64(long x) {
         return hash64(x, 100);
     }
-
-    /**
-     * Shrink the hash to a value 0..n. Kind of like modulo, but using
-     * multiplication and shift, which are faster to compute.
-     *
-     * @param hash the hash
-     * @param n the maximum of the result
-     * @return the reduced value
-     */
-    public static int reduce(long hash, int n) {
-        // http://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
-        return (int) (((hash & 0xffffffffL) * n) >>> 32);
-    }
-
 }
