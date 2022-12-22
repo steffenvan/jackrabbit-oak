5aab03ef6e14c4eead0736964483f11e52ce78de
steffenvan <svan@adobe.com>
2022-12-22 16:45:40 +0100

    refactor: added TestUtility class that manages the repository for test cases

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/NodeReaderTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/NodeReaderTest.java
@@ -1,59 +1,33 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
-import org.apache.jackrabbit.JcrConstants;
-import org.apache.jackrabbit.oak.InitialContent;
-import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
-import org.apache.jackrabbit.oak.api.ContentRepository;
-import org.apache.jackrabbit.oak.api.ContentSession;
-import org.apache.jackrabbit.oak.api.Root;
-import org.apache.jackrabbit.oak.api.Tree;
-import org.apache.jackrabbit.oak.plugins.index.AsyncIndexUpdate;
-import org.apache.jackrabbit.oak.plugins.index.IndexConstants;
-import org.apache.jackrabbit.oak.plugins.index.IndexUtils;
-import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
-import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
-import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
-import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
-import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
-import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
-import org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.jcr.NoSuchWorkspaceException;
 import javax.security.auth.login.LoginException;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 
-import static org.apache.jackrabbit.oak.api.Type.NAME;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NODE_TYPE;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class NodeReaderTest {
     NodeStore store;
-    Whiteboard wb;
-    ContentSession session;
-    Root root;
+    TestUtility utility;
 
     @Before
     public void before() throws CommitFailedException,
             NoSuchWorkspaceException, LoginException {
         store = new MemoryNodeStore();
-        session = createContentRepository().login(null, null);
-        root = session.getLatestRoot();
+        utility = new TestUtility(store, "statistics");
     }
 
     @Test
     public void testGetStatisticsIndexNode() throws CommitFailedException {
-        NodeState indexNode = NodeReader.getIndexNode(store);
+        NodeState indexNode = NodeReader.getIndexRoot(store);
         Optional<NodeState> statNode =
                 NodeReader.getStatisticsIndexDataNodeOrNull(
                 indexNode);
@@ -61,8 +35,8 @@ public class NodeReaderTest {
         // no properties added so it should be empty
         assertFalse(statNode.isPresent());
 
-        addProperties();
-        indexNode = NodeReader.getIndexNode(store);
+        utility.addNodes();
+        indexNode = NodeReader.getIndexRoot(store);
         statNode = NodeReader.getStatisticsIndexDataNodeOrNull(indexNode);
 
         // since we added some properties and re-read the index it should now
@@ -71,55 +45,25 @@ public class NodeReaderTest {
         assertTrue(statNode.isPresent() && statNode.get().exists());
     }
 
-    private void addProperties() throws CommitFailedException {
-        // add some properties so that the statistics node gets updated
-        for (int i = 0; i < 5; i++) {
-            Tree t = root.getTree("/").addChild("foo" + i);
-            for (int j = 0; j < 2; j++) {
-                t.addChild("bar" + j);
-            }
+    @Test
+    public void testGetStringOrEmptyWithValidString() throws CommitFailedException {
+        utility.addNodes();
+        NodeState indexNode = NodeReader.getIndexRoot(store);
+        Optional<NodeState> statNode =
+                NodeReader.getStatisticsIndexDataNodeOrNull(
+                indexNode);
 
-            root.commit();
-            runAsyncIndex();
-        }
+        assertTrue(statNode.isPresent());
+        NodeState statNodeIndex = statNode.get();
+        // since some nodes have been added we know that a property sketch
+        // has been created with at least one row
+        String val = NodeReader.getStringOrEmpty(statNodeIndex,
+                                                 StatisticsEditor.PROPERTY_CMS_NAME + 0);
+        assertFalse(val.isEmpty());
     }
 
-    private ContentRepository createContentRepository() throws CommitFailedException {
-        store = new MemoryNodeStore();
-        Oak oak = new Oak(store).with(new InitialContent())
-                                .with(new OpenSecurityProvider())
-                                .with(new PropertyIndexEditorProvider())
-                                .with(new StatisticsEditorProvider())
-                                // Effectively disable async indexing
-                                // auto run such that we can control run
-                                // timing as per test requirement
-                                .withAsyncIndexing("async",
-                                                   TimeUnit.DAYS.toSeconds(1));
+    @Test
+    public void testSetStatNodeWithInvalidIndex() {
 
-        //        mergeIndex("statistics");
-        NodeBuilder builder = store.getRoot().builder();
-        NodeBuilder index = IndexUtils.getOrCreateOakIndex(builder);
-
-        index.child("statistics")
-             .setProperty(JcrConstants.JCR_PRIMARYTYPE,
-                          INDEX_DEFINITIONS_NODE_TYPE, NAME)
-             .setProperty(TYPE_PROPERTY_NAME, StatisticsEditorProvider.TYPE)
-             .setProperty(IndexConstants.ASYNC_PROPERTY_NAME,
-                          IndexConstants.ASYNC_PROPERTY_NAME)
-             .setProperty("info", "STATISTICS");
-
-        store.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-        wb = oak.getWhiteboard();
-
-        return oak.createContentRepository();
     }
-
-    private void runAsyncIndex() {
-        Runnable async = WhiteboardUtils.getService(wb, Runnable.class,
-                                                    (Predicate<Runnable>) input -> input instanceof AsyncIndexUpdate);
-        assertNotNull(async);
-        async.run();
-        root.refresh();
-    }
-
 }
--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/StatisticsIndexTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/StatisticsIndexTest.java
@@ -1,53 +1,29 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
-import org.apache.jackrabbit.JcrConstants;
-import org.apache.jackrabbit.oak.InitialContent;
-import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
-import org.apache.jackrabbit.oak.api.ContentRepository;
-import org.apache.jackrabbit.oak.api.ContentSession;
-import org.apache.jackrabbit.oak.api.QueryEngine;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
 import org.apache.jackrabbit.oak.commons.json.JsonObject;
 import org.apache.jackrabbit.oak.commons.json.JsopTokenizer;
-import org.apache.jackrabbit.oak.plugins.index.AsyncIndexUpdate;
-import org.apache.jackrabbit.oak.plugins.index.IndexConstants;
-import org.apache.jackrabbit.oak.plugins.index.IndexUtils;
-import org.apache.jackrabbit.oak.plugins.index.counter.NodeCounterEditorProvider;
-import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
-import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
-import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
-import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
-import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStateUtils;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
-import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
-import org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 
-import static org.apache.jackrabbit.oak.api.Type.NAME;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NODE_TYPE;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
 import static org.apache.jackrabbit.oak.spi.state.AbstractNodeState.getLong;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class StatisticsIndexTest {
 
-    Whiteboard wb;
     NodeStore nodeStore;
     Root root;
-    QueryEngine qe;
-    ContentSession session;
+
+    private TestUtility utility;
 
     private static JsonObject parseJson(String json) {
         JsopTokenizer t = new JsopTokenizer(json);
@@ -57,55 +33,29 @@ public class StatisticsIndexTest {
 
     @Before
     public void before() throws Exception {
-        session = createRepository().login(null, null);
-        root = session.getLatestRoot();
-        qe = root.getQueryEngine();
-    }
-
-    private void addNodes() throws CommitFailedException {
-        // add some properties so that the statistics node gets updated
-        for (int i = 0; i < 5; i++) {
-            Tree t = root.getTree("/").addChild("foo" + i);
-            for (int j = 0; j < 2; j++) {
-                t.addChild("bar" + j);
-            }
-
-            root.commit();
-            runAsyncIndex();
-        }
-    }
-
-    private void addPropertyWithLongName() throws CommitFailedException {
-        for (int i = 0; i < 5; i++) {
-            Tree t = root.getTree("/").addChild("foo" + i);
-            for (int j = 0; j < 2; j++) {
-                t.addChild("bar".repeat(50));
-            }
-
-            root.commit();
-            runAsyncIndex();
-        }
+        nodeStore = new MemoryNodeStore();
+        utility = new TestUtility(nodeStore, "statistics");
     }
 
     @Test
     public void testCorruptedIndex() throws CommitFailedException {
-        addNodes();
-        Tree t = getIndexNodeTree();
+        utility.addNodes();
+        Tree t = utility.getIndexNodeTree();
 
         //        builder.setProperty();
     }
 
     @Test
     public void testLongValueName() throws CommitFailedException {
-        addPropertyWithLongName();
-        Tree t = getIndexNodeTree();
+        utility.addPropertyWithLongName();
+        Tree t = utility.getIndexNodeTree();
 
         //        assertTrue(t)
     }
 
     @Test
     public void testPropertySketchIsStored() throws CommitFailedException {
-        NodeState indexNode = NodeReader.getIndexNode(nodeStore);
+        NodeState indexNode = NodeReader.getIndexRoot(nodeStore);
         Optional<NodeState> statNode =
                 NodeReader.getStatisticsIndexDataNodeOrNull(
                 indexNode);
@@ -113,8 +63,8 @@ public class StatisticsIndexTest {
         // no properties added so it should be empty
         assertFalse(statNode.isPresent());
 
-        addNodes();
-        indexNode = NodeReader.getIndexNode(nodeStore);
+        utility.addNodes();
+        indexNode = NodeReader.getIndexRoot(nodeStore);
         statNode = NodeReader.getStatisticsIndexDataNodeOrNull(indexNode);
 
         // since we added *some* properties (note that it doesn't really matter
@@ -138,17 +88,12 @@ public class StatisticsIndexTest {
 
     }
 
-    private Tree getIndexNodeTree() {
-        String path = "/oak:index/statistics/index";
-        return root.getTree(path);
-    }
-
     @Test
     public void testPropertyCMSIsStored() throws Exception {
-        addNodes();
+        utility.addNodes();
 
 
-        Tree t = getIndexNodeTree();
+        Tree t = utility.getIndexNodeTree();
         // the index should be created after we add the properties.
         assertTrue(nodeExists(t.getPath()));
 
@@ -165,54 +110,11 @@ public class StatisticsIndexTest {
         }
 
         t.remove();
-        root.commit();
+        utility.commit();
         assertFalse(nodeExists(t.getPath()));
     }
 
     private boolean nodeExists(String path) {
         return NodeStateUtils.getNode(nodeStore.getRoot(), path).exists();
-    }
-
-    protected ContentRepository createRepository() throws CommitFailedException {
-        nodeStore = new MemoryNodeStore();
-        Oak oak = new Oak(nodeStore).with(new InitialContent())
-                                    .with(new OpenSecurityProvider())
-                                    .with(new PropertyIndexEditorProvider())
-                                    .with(new StatisticsEditorProvider())
-                                    .with(new NodeCounterEditorProvider())
-                                    // Effectively disable async indexing
-                                    // auto run such that we can control run
-                                    // timing as per test requirement
-                                    .withAsyncIndexing("async",
-                                                       TimeUnit.DAYS.toSeconds(
-                                                               1));
-
-        mergeIndex("statistics");
-        wb = oak.getWhiteboard();
-
-        return oak.createContentRepository();
-    }
-
-    private void mergeIndex(String name) throws CommitFailedException {
-        NodeBuilder builder = nodeStore.getRoot().builder();
-        NodeBuilder index = IndexUtils.getOrCreateOakIndex(builder);
-
-        index.child(name)
-             .setProperty(JcrConstants.JCR_PRIMARYTYPE,
-                          INDEX_DEFINITIONS_NODE_TYPE, NAME)
-             .setProperty(TYPE_PROPERTY_NAME, StatisticsEditorProvider.TYPE)
-             .setProperty(IndexConstants.ASYNC_PROPERTY_NAME,
-                          IndexConstants.ASYNC_PROPERTY_NAME)
-             .setProperty("info", "STATISTICS");
-
-        nodeStore.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-    }
-
-    private void runAsyncIndex() {
-        Runnable async = WhiteboardUtils.getService(wb, Runnable.class,
-                                                    (Predicate<Runnable>) input -> input instanceof AsyncIndexUpdate);
-        assertNotNull(async);
-        async.run();
-        root.refresh();
     }
 }
