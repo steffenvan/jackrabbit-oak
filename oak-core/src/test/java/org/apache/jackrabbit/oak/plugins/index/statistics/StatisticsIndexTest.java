fd0df6fd9d78f1837416eb449db35d5442f21e7a
steffenvan <svan@adobe.com>
2022-12-22 14:47:47 +0100

    refactor: simplified  statistics index while maintaining the core logic

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/StatisticsIndexTest.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/StatisticsIndexTest.java
@@ -1,28 +1,12 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics;
 
-import static org.apache.jackrabbit.oak.api.QueryEngine.NO_MAPPINGS;
-import static org.apache.jackrabbit.oak.api.Type.NAME;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NODE_TYPE;
-import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.text.ParseException;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
-
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.InitialContent;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.api.CommitFailedException;
 import org.apache.jackrabbit.oak.api.ContentRepository;
 import org.apache.jackrabbit.oak.api.ContentSession;
-import org.apache.jackrabbit.oak.api.PropertyValue;
 import org.apache.jackrabbit.oak.api.QueryEngine;
-import org.apache.jackrabbit.oak.api.Result;
-import org.apache.jackrabbit.oak.api.ResultRow;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
 import org.apache.jackrabbit.oak.commons.json.JsonObject;
@@ -30,213 +14,205 @@ import org.apache.jackrabbit.oak.commons.json.JsopTokenizer;
 import org.apache.jackrabbit.oak.plugins.index.AsyncIndexUpdate;
 import org.apache.jackrabbit.oak.plugins.index.IndexConstants;
 import org.apache.jackrabbit.oak.plugins.index.IndexUtils;
+import org.apache.jackrabbit.oak.plugins.index.counter.NodeCounterEditorProvider;
 import org.apache.jackrabbit.oak.plugins.index.property.PropertyIndexEditorProvider;
 import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
 import org.apache.jackrabbit.oak.spi.commit.CommitInfo;
 import org.apache.jackrabbit.oak.spi.commit.EmptyHook;
 import org.apache.jackrabbit.oak.spi.security.OpenSecurityProvider;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.spi.state.NodeStateUtils;
 import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.apache.jackrabbit.oak.spi.whiteboard.Whiteboard;
 import org.apache.jackrabbit.oak.spi.whiteboard.WhiteboardUtils;
-import org.jetbrains.annotations.Nullable;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+import static org.apache.jackrabbit.oak.api.Type.NAME;
+import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.INDEX_DEFINITIONS_NODE_TYPE;
+import static org.apache.jackrabbit.oak.plugins.index.IndexConstants.TYPE_PROPERTY_NAME;
+import static org.apache.jackrabbit.oak.spi.state.AbstractNodeState.getLong;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class StatisticsIndexTest {
 
-	Whiteboard wb;
-	NodeStore nodeStore;
-	Root root;
-	QueryEngine qe;
-	ContentSession session;
+    Whiteboard wb;
+    NodeStore nodeStore;
+    Root root;
+    QueryEngine qe;
+    ContentSession session;
 
-	@Before
-	public void before() throws Exception {
-		session = createRepository().login(null, null);
-		root = session.getLatestRoot();
-		qe = root.getQueryEngine();
-	}
+    private static JsonObject parseJson(String json) {
+        JsopTokenizer t = new JsopTokenizer(json);
+        t.read('{');
+        return JsonObject.create(t);
+    }
 
-	@Test
-	public void testStatisticsAreStored() throws Exception {
-		String path = "/oak:index/statistics";
-//		Tree r = root.getTree(path);
-//		String pathToTest = "/index/properties/"
-		String pathToCount = "/index/properties/propertiesCountMinSketch";
-		String pathToHll = "/index/properties/uniqueHLL";
+    @Before
+    public void before() throws Exception {
+        session = createRepository().login(null, null);
+        root = session.getLatestRoot();
+        qe = root.getQueryEngine();
+    }
 
-//		root.commit();
-//		runAsyncIndex();
+    @After
+    public void after() throws Exception {
+        session = null;
+        root = null;
+        qe = null;
+    }
 
-		addNodesWithProperties(path, 10);
+    private void addProperties() throws CommitFailedException {
+        // add some properties so that the statistics node gets updated
+        for (int i = 0; i < 5; i++) {
+            Tree t = root.getTree("/").addChild("foo" + i);
+            for (int j = 0; j < 2; j++) {
+                t.addChild("bar" + j);
+            }
 
-//		root.getTree(pathToHll)
-		Tree cmsTree = root.getTree(path + pathToCount);
-		Tree hllTree = root.getTree(path + pathToHll);
+            root.commit();
+            runAsyncIndex();
+        }
+    }
 
-		assertTrue(nodeExists("oak:index/statistics/index"));
-		assertTrue(nodeExists(cmsTree.getPath()));
-		assertTrue(nodeExists(hllTree.getPath()));
+    private void addPropertyWithLongName() throws CommitFailedException {
+        for (int i = 0; i < 5; i++) {
+            Tree t = root.getTree("/").addChild("foo" + i);
+            for (int j = 0; j < 2; j++) {
+                t.addChild("bar".repeat(50));
+            }
 
-	}
+            root.commit();
+            runAsyncIndex();
+        }
+    }
 
-	private void addNodesWithProperties(String path, int maxIter) throws CommitFailedException {
-		Random r = new Random();
-		int maxNum = 10;
-		int num = r.nextInt(maxNum);
-//		data[i] = r.nextInt(1 << num);
-		for (int i = 0; i < 10; i++) {
-			assertTrue("index not ready after " + maxIter + " iterations", i < maxIter);
-			Tree t = root.getTree(path).addChild("test-" + i);
-			for (int j = 0; j < 5; j++) {
-				t.addChild("prop-" + j).setProperty("ABC", r.nextInt(10));
-			}
-			root.commit();
-			runAsyncIndex();
-		}
-	}
+    @Test
+    public void testLongValueName() throws CommitFailedException {
+        addPropertyWithLongName();
+        Tree t = getIndexNodeTree();
 
-	@Test
-	public void testNotUsedBeforeValid() throws Exception {
-		String path = "/oak:index/statistics";
-		root.getTree(path).setProperty("resolution", 100);
-		root.commit();
+        //        assertTrue(t)
+    }
 
-		// no index data before indexing
-		assertFalse(nodeExists("oak:index/statistics/index"));
-		assertTrue(nodeExists("oak:index/statistics"));
-		// so, cost for traversal is high
-		assertTrue(getCost("/jcr:root//*") >= 1.0E8);
+    @Test
+    public void testPropertySketchIsStored() throws CommitFailedException {
+        NodeState indexNode = NodeReader.getIndexNode(nodeStore);
+        Optional<NodeState> statNode =
+                NodeReader.getStatisticsIndexDataNodeOrNull(
+                indexNode);
 
-		runAsyncIndex();
-		// sometimes, the :index node doesn't exist because there are very few
-		// nodes (randomly, because the seed value of the node statistics is random
-		// by design) - so we create nodes until the index exists
-		// (we could use a fixed seed to ensure this is not the case,
-		// but creating nodes has the same effect)
-		String s = root.getTree(path + "/index/properties/jcr:valueConstraints").toString();
-//        System.out.println(s);
-//        System.out.println("hello");
+        // no properties added so it should be empty
+        assertFalse(statNode.isPresent());
 
-		System.out.println(nodeExists("oak:index/statistics/index"));
-		int maxIter = 100;
-		for (int i = 0; !nodeExists("oak:index/statistics/index"); i++) {
-			assertTrue("index not ready after " + maxIter + " iterations", i < maxIter);
-			Tree t = root.getTree(path).addChild("test" + i);
-			for (int j = 0; j < 100; j++) {
-				t.addChild("n" + j).setProperty("ABC", 1);
-				;
-//                System.out.println("HELLO");
-			}
-			root.commit();
-			runAsyncIndex();
-		}
+        addProperties();
+        indexNode = NodeReader.getIndexNode(nodeStore);
+        statNode = NodeReader.getStatisticsIndexDataNodeOrNull(indexNode);
 
-		for (int i = 0; i < 5; i++) {
-			assertTrue("index not ready after " + maxIter + " iterations", i < maxIter);
-			Tree t = root.getTree(path).addChild("test-" + i);
-			for (int j = 0; j < 100; j++) {
-				t.addChild("ns" + j).setProperty("ABC", j);
-			}
-			root.commit();
-			runAsyncIndex();
-		}
+        // since we added *some* properties (note that it doesn't really matter
+        // which ones) and re-read the index it should now exist.
+        assertTrue(statNode.isPresent() && statNode.get().exists());
 
-		for (int i = 0; i < 5; i++) {
-			assertTrue("index not ready after " + maxIter + " iterations", i < maxIter);
-			Tree t = root.getTree(path).addChild("test2-" + i);
-			for (int j = 0; j < 420; j++) {
-				t.addChild("ns" + j).setProperty("KFC", j);
-			}
-			root.commit();
-			runAsyncIndex();
-		}
-		// TODO: check that the tree follows the desired structure.
-//        String s = root.getTree(path).toString();
+        NodeState statIndexNode = statNode.get();
 
-		// remove the statistics index
-		root.getTree(path).remove();
-		root.commit();
+        System.out.println(statIndexNode);
+        assertTrue(statIndexNode.hasProperty(
+                StatisticsEditor.PROPERTY_CMS_ROWS_NAME));
+        assertTrue(statIndexNode.hasProperty(
+                StatisticsEditor.PROPERTY_CMS_COLS_NAME));
 
-		assertFalse(nodeExists(path));
-	}
+        int rows = Math.toIntExact(getLong(statIndexNode,
+                                           StatisticsEditor.PROPERTY_CMS_ROWS_NAME));
+        for (int i = 0; i < rows; i++) {
+            assertTrue(statIndexNode.hasProperty(
+                    StatisticsEditor.PROPERTY_CMS_NAME + i));
+        }
 
-	private double getCost(String xpath) throws ParseException {
-		String plan = executeXPathQuery("explain measure " + xpath);
-		String cost = plan.substring(plan.lastIndexOf('{'));
-		JsonObject json = parseJson(cost);
-		double c = Double.parseDouble(json.getProperties().get("a"));
-		return c;
-	}
+    }
 
-	private static JsonObject parseJson(String json) {
-		JsopTokenizer t = new JsopTokenizer(json);
-		t.read('{');
-		return JsonObject.create(t);
-	}
+    private Tree getIndexNodeTree() {
+        String path = "/oak:index/statistics/index";
+        return root.getTree(path);
+    }
 
-	private boolean nodeExists(String path) {
-		return NodeStateUtils.getNode(nodeStore.getRoot(), path).exists();
-	}
+    @Test
+    public void testPropertyCMSIsStored() throws Exception {
+        addProperties();
 
-	protected String executeXPathQuery(String statement) throws ParseException {
-		Result result = qe.executeQuery(statement, "xpath", null, NO_MAPPINGS);
-		StringBuilder buff = new StringBuilder();
-		for (ResultRow row : result.getRows()) {
-			for (PropertyValue v : row.getValues()) {
-				buff.append(v);
-			}
-		}
-		return buff.toString();
-	}
 
-	protected ContentRepository createRepository() throws CommitFailedException {
-		nodeStore = new MemoryNodeStore();
-		Oak oak = new Oak(nodeStore).with(new InitialContent()).with(new OpenSecurityProvider())
-				.with(new PropertyIndexEditorProvider()).with(new StatisticsEditorProvider())
-//                .with(new NodeCounterEditorProvider())
-				// Effectively disable async indexing auto run
-				// such that we can control run timing as per test requirement
-				.withAsyncIndexing("async", TimeUnit.DAYS.toSeconds(1));
+        Tree t = getIndexNodeTree();
+        // the index should be created after we add the properties.
+        assertTrue(nodeExists(t.getPath()));
 
-//        mergeIndex("statistics");
-		NodeBuilder builder = nodeStore.getRoot().builder();
-		NodeBuilder index = IndexUtils.getOrCreateOakIndex(builder);
+        assertTrue(t.hasProperty(StatisticsEditor.PROPERTY_CMS_ROWS_NAME));
+        assertTrue(t.hasProperty(StatisticsEditor.PROPERTY_CMS_COLS_NAME));
 
-		index.child("statistics").setProperty(JcrConstants.JCR_PRIMARYTYPE, INDEX_DEFINITIONS_NODE_TYPE, NAME)
-				.setProperty(TYPE_PROPERTY_NAME, StatisticsEditorProvider.TYPE)
-				.setProperty(IndexConstants.ASYNC_PROPERTY_NAME, IndexConstants.ASYNC_PROPERTY_NAME)
-				.setProperty("info", "STATISTICS");
+        // since we aren't doing any modifications to the construction of the
+        // Count-Min sketch, we know that the one that the one that is
+        // created after we add the properties will have the default number
+        // of rows
+        int rows = StatisticsEditorProvider.DEFAULT_PROPERTY_CMS_ROWS;
+        for (int i = 0; i < rows; i++) {
+            assertTrue(t.hasProperty(StatisticsEditor.PROPERTY_CMS_NAME + i));
+        }
 
-		nodeStore.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-		wb = oak.getWhiteboard();
+        t.remove();
+        root.commit();
+        assertFalse(nodeExists(t.getPath()));
+    }
 
-		return oak.createContentRepository();
-	}
+    private boolean nodeExists(String path) {
+        return NodeStateUtils.getNode(nodeStore.getRoot(), path).exists();
+    }
 
-	private void mergeIndex(String name) throws CommitFailedException {
-		NodeBuilder builder = nodeStore.getRoot().builder();
-		NodeBuilder index = IndexUtils.getOrCreateOakIndex(builder);
+    protected ContentRepository createRepository() throws CommitFailedException {
+        nodeStore = new MemoryNodeStore();
+        Oak oak = new Oak(nodeStore).with(new InitialContent())
+                                    .with(new OpenSecurityProvider())
+                                    .with(new PropertyIndexEditorProvider())
+                                    .with(new StatisticsEditorProvider())
+                                    .with(new NodeCounterEditorProvider())
+                                    // Effectively disable async indexing
+                                    // auto run such that we can control run
+                                    // timing as per test requirement
+                                    .withAsyncIndexing("async",
+                                                       TimeUnit.DAYS.toSeconds(
+                                                               1));
 
-		index.child(name).setProperty(JcrConstants.JCR_PRIMARYTYPE, INDEX_DEFINITIONS_NODE_TYPE, NAME)
-				.setProperty(TYPE_PROPERTY_NAME, StatisticsEditorProvider.TYPE)
-				.setProperty(IndexConstants.ASYNC_PROPERTY_NAME, IndexConstants.ASYNC_PROPERTY_NAME)
-				.setProperty("info", "STATISTICS");
+        mergeIndex("statistics");
+        wb = oak.getWhiteboard();
 
-		nodeStore.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
-	}
+        return oak.createContentRepository();
+    }
 
-	private void runAsyncIndex() {
-		Runnable async = WhiteboardUtils.getService(wb, Runnable.class, new Predicate<Runnable>() {
-			@Override
-			public boolean test(@Nullable Runnable input) {
-				return input instanceof AsyncIndexUpdate;
-			}
-		});
-		assertNotNull(async);
-		async.run();
-		root.refresh();
-	}
+    private void mergeIndex(String name) throws CommitFailedException {
+        NodeBuilder builder = nodeStore.getRoot().builder();
+        NodeBuilder index = IndexUtils.getOrCreateOakIndex(builder);
+
+        index.child(name)
+             .setProperty(JcrConstants.JCR_PRIMARYTYPE,
+                          INDEX_DEFINITIONS_NODE_TYPE, NAME)
+             .setProperty(TYPE_PROPERTY_NAME, StatisticsEditorProvider.TYPE)
+             .setProperty(IndexConstants.ASYNC_PROPERTY_NAME,
+                          IndexConstants.ASYNC_PROPERTY_NAME)
+             .setProperty("info", "STATISTICS");
+
+        nodeStore.merge(builder, EmptyHook.INSTANCE, CommitInfo.EMPTY);
+    }
+
+    private void runAsyncIndex() {
+        Runnable async = WhiteboardUtils.getService(wb, Runnable.class,
+                                                    (Predicate<Runnable>) input -> input instanceof AsyncIndexUpdate);
+        assertNotNull(async);
+        async.run();
+        root.refresh();
+    }
 }
