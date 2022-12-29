7dba8938de7ed41c3ecca08bace3143212cb58ce
steffenvan <svan@adobe.com>
2022-12-29 23:21:33 +0100

    chore: removed unused functions

--- oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/state/export/NodeStateExporter.java
+++ oak-core/src/test/java/org/apache/jackrabbit/oak/plugins/index/statistics/state/export/NodeStateExporter.java
@@ -1,24 +1,11 @@
 package org.apache.jackrabbit.oak.plugins.index.statistics.state.export;
 
-import java.util.Map;
-
 import org.apache.jackrabbit.oak.spi.state.NodeState;
 
+import java.util.Map;
+
 public class NodeStateExporter {
-
-    public static String toJson(NodeState state) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
     public static Map<String, Object> toMap(NodeState state) {
         return new JsonExporter().toMap(state);
     }
-
-//    static String toCND(NodeState state){
-//        return new CndExporter().toCNDFormat(state)
-//    }
-//
-//    static String toXml(NodeState state){
-//        return new XmlExporter().toXml(state)
-//    }
 }
